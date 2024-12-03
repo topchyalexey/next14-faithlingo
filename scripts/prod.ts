f9db768cd085e78e664604dfaf2d16ce1b224b84
aleksey.topchy <Aleksey.Topchy@x5.ru>
2024-12-03 20:00:19 +0300

    Server PG

--- scripts/prod.ts
+++ scripts/prod.ts
@@ -1,12 +1,22 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
 
+
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "../db/schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-// @ts-ignore
-const db = drizzle(sql, { schema });
+import { Pool } from 'pg';
+
+// Configure PostgreSQL client
+const pool = new Pool({
+  user: process.env.POSTGRESQL_USER,           // PostgreSQL user
+  host: process.env.POSTGRESQL_HOST,            // Database host
+  database: process.env.POSTGRESQL_DBNAME,    // Database name
+  password: process.env.POSTGRESQL_PASSWORD,    // Database password
+  port: Number(process.env.POSTGRESQL_PORT),                   // PostgreSQL port
+});
+
+// Connect Drizzle to the PostgreSQL client
+const db = drizzle(pool, { schema });
 
 const main = async () => {
   try {
